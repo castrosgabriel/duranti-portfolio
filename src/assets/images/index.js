095ea7f124e9cb2af1508a4b6488d6f0209b496c
castrosgabriel <castro.sgabriel@gmail.com>
2024-02-06 02:41:38 -0300

    Clean repositoty 2

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,18 +1,6 @@
 
-import RennerGamesImage from './renner-games.png';
-import RennerKidsImage from './renner-kids.png';
-import RennerPlanetaImage from './renner-planeta.png';
-import DistritoJazzImage from './distrito-jazz.png';
-import NatalImage from './natal.png';
-import RennerNostalgiaImage from './renner-nostalgia.png';
 import DurantVectorImage from './durant-vector.png';
 
 export {
-    RennerGamesImage,
-    RennerKidsImage,
-    RennerPlanetaImage,
-    DistritoJazzImage,
-    NatalImage,
-    RennerNostalgiaImage,
     DurantVectorImage
 };
